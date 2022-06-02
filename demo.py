521482ac4aea51a8f756fee93c725594daa372a3
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2022-06-02 20:20:16 +0500

    Move CsvDataset to separate module

--- demo.py
+++ demo.py
@@ -5,9 +5,8 @@ import gc
 import sys
 import datetime as dt
 
-from csv import DictReader
-from typing import List, Callable, Optional
 from functools import partial
+from src.dataset import CsvDataset
 from src.preproc import Preprocessor, LangEnum
 from src.models import Top2VecW
 from tqdm.notebook import tqdm
@@ -18,76 +17,6 @@ if len(sys.argv) != 3:
 
 DATASET_PATH = sys.argv[1]
 OUTPUT_DIR = sys.argv[2]
-
-
-class CsvDataset:
-    """
-    Dataloader that yields batches from csv using tumbling time window
-    """
-
-    def __init__(self, fname: str):
-        self.fname = fname
-
-    def __call__(self,
-                 batch_size: int = 0,
-                 time_col: str = "date",
-                 time_window: Optional[dt.timedelta] = None,
-                 columns: Optional[List[str]] = None,
-                 time_format: Optional[str] = None,
-                 drop_last: bool = True,
-                 text_column: str = "text",
-                 max_size: int = 0,
-                 filter_func: Optional[Callable[dict, bool]] = None):
-        batch = []
-        start_time = None
-
-        assert (time_window is not None and time_col is not None
-                and time_format is not None) or batch_size != 0
-
-        if filter_func is None:
-            # Use a stub
-            filter_func = (lambda _: True)
-
-        with open(self.fname, "r") as fd:
-            to_yield = False
-            reader = DictReader(fd)
-
-            for line in reader:
-                cur_time = dt.datetime.strptime(line[time_col],
-                                                time_format).date()
-
-                if start_time is None:
-                    batch = []
-                    start_time = cur_time
-
-                if len(batch) > 0:
-                    # Split for readability
-                    if time_window is not None and \
-                            cur_time > start_time + time_window:
-                        to_yield = True
-                    elif batch_size != 0 and len(batch) == batch_size:
-                        to_yield = True
-                    elif max_size > 0 and len(batch == max_size):
-                        to_yield = True
-
-                if to_yield:
-                    start_time = None
-                    to_yield = False
-                    yield batch
-                else:
-                    # Filter out duplicates
-                    last_text = batch[-1][text_column]
-                    cur_text = line[text_column]
-
-                    strings_equal = last_text == cur_text
-
-                    if len(batch) > 0 and strings_equal or len(batch) == 0:
-                        if filter_func(line):
-                            batch.append({key: line[key] for key in columns})
-
-        if len(batch) > 0 and not drop_last:
-            yield batch
-        raise StopIteration
 
 
 def crutch_for_top2vec(doc):
---
+++ src/dataset.py
@@ -0,0 +1,73 @@
+import datetime as dt
+from csv import DictReader
+from typing import Optional, Callable, List
+
+
+class CsvDataset:
+    """
+    Dataloader that yields batches from csv using tumbling time window
+    """
+
+    def __init__(self, fname: str):
+        self.fname = fname
+
+    def __call__(self,
+                 batch_size: int = 0,
+                 time_col: str = "date",
+                 time_window: Optional[dt.timedelta] = None,
+                 columns: Optional[List[str]] = None,
+                 time_format: Optional[str] = None,
+                 drop_last: bool = True,
+                 text_column: str = "text",
+                 max_size: int = 0,
+                 filter_func: Optional[Callable[dict, bool]] = None):
+        batch = []
+        start_time = None
+
+        assert (time_window is not None and time_col is not None
+                and time_format is not None) or batch_size != 0
+
+        if filter_func is None:
+            # Use a stub
+            filter_func = (lambda _: True)
+
+        with open(self.fname, "r") as fd:
+            to_yield = False
+            reader = DictReader(fd)
+
+            for line in reader:
+                cur_time = dt.datetime.strptime(line[time_col],
+                                                time_format).date()
+
+                if start_time is None:
+                    batch = []
+                    start_time = cur_time
+
+                if len(batch) > 0:
+                    # Split for readability
+                    if time_window is not None and \
+                            cur_time > start_time + time_window:
+                        to_yield = True
+                    elif batch_size != 0 and len(batch) == batch_size:
+                        to_yield = True
+                    elif max_size > 0 and len(batch == max_size):
+                        to_yield = True
+
+                if to_yield:
+                    start_time = None
+                    to_yield = False
+                    yield batch
+                else:
+                    # Filter out duplicates
+                    last_text = batch[-1][text_column]
+                    cur_text = line[text_column]
+
+                    strings_equal = last_text == cur_text
+
+                    if len(batch) > 0 and strings_equal or len(batch) == 0:
+                        if filter_func(line):
+                            batch.append({key: line[key] for key in columns})
+
+        if len(batch) > 0 and not drop_last:
+            yield batch
+        raise StopIteration
